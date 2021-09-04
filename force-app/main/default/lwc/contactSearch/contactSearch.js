62c4de9e62b419858210fb5f478e1cd25f3617f0
Mohith Kumar <mohithcena66@gmail.com>
2021-09-04 11:52:19 +0530

    contact lightning form

--- force-app/main/default/lwc/contactSearch/contactSearch.js
+++ force-app/main/default/lwc/contactSearch/contactSearch.js
@@ -1,16 +1,23 @@
 import { LightningElement, wire } from 'lwc';
 import getAllContacts from '@salesforce/apex/ContactSearchController.getAllContacts'
 
-export default class ContactsLWC extends LightningElement {
-    @wire(getAllContacts) wiredContacts; //These will be automatically available if successful
+export default class contactSearch extends LightningElement {
+    @wire(getAllContacts) wiredContacts;
     getContacts() {
         getAllContacts()
         .then(contacts => {
-            //console.log(JSON.stringify(contacts))
             console.log('Got Contacts: ' + contacts.length);
         })
         .catch(error => {
             console.log(error)
         });
+    }
+    value = '';
+
+    get options() {
+        return [
+            { label: 'All Contacts', value: 'option1' },
+            { label: 'Search Pattern', value: 'option2' },
+        ];
     }
 }
